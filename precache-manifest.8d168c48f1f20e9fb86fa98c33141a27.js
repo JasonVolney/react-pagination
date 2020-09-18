fa10c64d676346739a567caa9c7c41431ac3c14e
JasonVolney <jasonvolneysilva@gmail.com>
2020-09-18 13:37:48 -0300

    Updates

---
+++ precache-manifest.8d168c48f1f20e9fb86fa98c33141a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d7d414e88bf5c5c69fb686161abe3a",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "d888d15612cb69715aa1",
+    "url": "/react-pagination/static/css/main.e59e26eb.chunk.css"
+  },
+  {
+    "revision": "b7c114592ef1eb1c6cd6",
+    "url": "/react-pagination/static/js/2.c65270d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-pagination/static/js/2.c65270d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d888d15612cb69715aa1",
+    "url": "/react-pagination/static/js/main.9dd282e9.chunk.js"
+  },
+  {
+    "revision": "0de06f4cbc7bf524b0cc",
+    "url": "/react-pagination/static/js/runtime-main.de79ddfe.js"
+  }
+]);
